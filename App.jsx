841a5d8ade1ca99d167a8bf251fb5e272c186537
codesimran99 <46073037+codesimran99@users.noreply.github.com>
2021-06-07 08:06:18 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>Shape AI 7 Days React and JavaScript Bootcamp</h1>
+      <p>
+        This project is during 7 day javascript and react js bootcamp conducted{" "}
+        <b>Shape AI</b>
+      </p>
+      <p>
+        The instructor of this bootcamp was Shaurya Sinha (Data Analyst Intern
+        at Jio)
+      </p>
+      <p>In this 7 day bootcamp, we get to learn-</p>
+      <ul>
+        <li>JavaScript</li>
+        <li>HTML</li>
+        <li>React.js</li>
+      </ul>
+      <p>
+        This was my first time attending any online event and it was amazing.
+        Cant wait for more
+      </p>
+    </div>
+  );
+}
+export default Note;
